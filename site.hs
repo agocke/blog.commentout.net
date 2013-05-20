fe27efe9a6cbdd8d0039eeaa5c0947369a8ba580
Andy Gocke <andy@commentout.net>
2013-05-19 20:38:47 -0700

    Refactoring site.hs

--- site.hs
+++ site.hs
@@ -8,52 +8,53 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      match "images/*" $ do
+          route   idRoute
+          compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      match "css/*" $ do
+          route   idRoute
+          compile compressCssCompiler
 
-    match (fromList ["welcome.markdown", "about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+      match (fromList pages) $ do
+          route   $ setExtension ".html"
+          compile $ pandocCompiler
+              >>= loadAndApplyTemplate "templates/default.html" defaultContext
+              >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+      match "posts/*" $ do
+          route $ setExtension "html"
+          compile $ pandocCompiler
+              >>= loadAndApplyTemplate "templates/post.html"    postCtx
+              >>= loadAndApplyTemplate "templates/default.html" postCtx
+              >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            let archiveCtx =
-                    field "posts" (\_ -> postList recentFirst) `mappend`
-                    constField "title" "Archives"              `mappend`
-                    defaultContext
+      create ["archive.html"] $ do
+          route idRoute
+          compile $ do
+              let archiveCtx =
+                      field "posts" (\_ -> postList recentFirst) `mappend`
+                      constField "title" "Archives"              `mappend`
+                      defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+              makeItem ""
+                  >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                  >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                  >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
+      match "index.html" $ do
+          route idRoute
+          compile $ do
+              let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
-                >>= relativizeUrls
+              getResourceBody
+                  >>= applyAsTemplate indexCtx
+                  >>= loadAndApplyTemplate "templates/default.html" postCtx
+                  >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
+      match "templates/*" $ compile templateCompiler
 
+    where pages = [ "about.markdown" ]
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
